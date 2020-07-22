e536c44aef23b5e541526d99e049bcc3dff97e8b
yq <yongquan_teo@mymail.sutd.edu.sg>
2020-07-23 04:29:05 +0800

    Tower first attempt

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject bulletPrefab;
+    public int atkRange;
+    void Start()
+    {
+         //rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(atkRange,0));
+        if (hit.collider.CompareTag("Enemy"))
+        {
+            
+            Instantiate(bulletPrefab, transform.position, transform.rotation);
+            
+
+        }
+    }
+}
